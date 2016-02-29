5575a9dc2fee0b13f39d6a1bde89a2157c3ea6f2
Carlos Jesús Sánchez <carlosjsanchezortega@gmail.com>
2016-02-29 14:16:23 +0100

    Eliminar estrellas del nombre a la hora de comparar el nombre o la cadena entrante

---
+++ enun5.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# 5. Pide por teclado el nombre 
+# de un hotel y te devuelva su pagina web (Nota: todos los nombres de los hoteles empiezan por el numero de * que tienen )
+
+import json
+
+with open("hoteles.json") as fichero:
+	datos = json.load(fichero)
+
+var = raw_input("Nombre del hotel o que empiece por tal cadena: ")
+
+for d in datos["resources"]:
+	if d["dc:title"].count("*") == 5:
+		hotel = d["dc:title"].lstrip("*****")
+		print hotel

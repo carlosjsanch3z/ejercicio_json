21b7923aef193c9fc920446d0e8014e98cdf575b
Carlos Jesús Sánchez <carlosjsanchezortega@gmail.com>
2016-03-01 20:17:31 +0100

    Realizar los pasos comentados en el fichero

--- enun5.py
+++ enun5.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 # 5. Pide por teclado el nombre 
-# de un hotel y te devuelva su pagina web (Nota: todos los nombres de los hoteles empiezan por el numero de * que tienen )
+# de un hotel y te devuelva su pagina web (Nota: casi todos los nombres de los hoteles empiezan por el numero de * que tienen )
 
 import json
 
@@ -11,8 +11,16 @@ with open("hoteles.json") as fichero:
 
 var = raw_input("Nombre del hotel o que empiece por tal cadena: ")
 
+#Recorrer y dejar los nombres sin sus estrellas
 for d in datos["resources"]:
 	if d["dc:title"].count("*") == 5:
 		d["dc:title"] = d["dc:title"].lstrip("*****")
 		if d["dc:title"].startswith(var):
 			print d["dc:title"]
+
+
+#Almacenarlos en una lista o diccionario
+
+
+
+#Buscar si la variable coincide con una key y mostrar su value = url
